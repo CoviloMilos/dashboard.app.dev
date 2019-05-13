bda9555b99bfb8574ded28b2d4a47d7871227cf8
Milos Covilo <milos.covilo@devoteam.com>
2019-05-13 11:30:47 +0200

    Created line chart with mocking data

--- ng-sight/src/app/charts/line-chart/line-chart.component.ts
+++ ng-sight/src/app/charts/line-chart/line-chart.component.ts
@@ -1,4 +1,13 @@
+import { LINE_CHART_COLORS } from './../../shared/chart.color';
 import { Component, OnInit } from '@angular/core';
+
+const LINE_CHART_SAMPLE_DATA: any[] = [
+  { data: [32, 14, 46, 23, 38, 56], label: 'Sentiment Analysis'},
+  { data: [12, 18, 26, 13, 28, 26], label: 'Image Recognition'},
+  { data: [52, 34, 49, 52, 68, 62], label: 'Forecasting'},
+];
+
+const LINE_CHART_LABLES: string[] = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'];
 
 @Component({
   selector: 'app-line-chart',
@@ -6,6 +15,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./line-chart.component.css']
 })
 export class LineChartComponent implements OnInit {
+
+  public lineChartData: any[] = LINE_CHART_SAMPLE_DATA;
+  public lineChartLabels: string[] = LINE_CHART_LABLES;
+  public lineChartType = 'line';
+  public lineChartLegend  = LINE_CHART_COLORS;
+  public lineChartOptions: any = {
+    scaleShowVerticalLines: false,
+    responsive: true
+  };
 
   constructor() { }
 
